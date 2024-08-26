e1a39a243c7829fa936b648e93278f67c3d1e83e
blozink <juavergara24@gmail.com>
2024-08-25 22:34:17 -0400

    Ajuste ruta local a 5000
    ajuste local de url

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -5,7 +5,7 @@ const Pizza = () => {
   const [pizza, setPizza] = useState(null); 
 
   useEffect(() => {
-    fetch('http://localhost:5001/api/pizzas/p001')
+    fetch('http://localhost:5000/api/pizzas/p001')
       .then(response => response.json())
       .then(data => setPizza(data))
       .catch(error => console.error('Error fetching pizza:', error));
